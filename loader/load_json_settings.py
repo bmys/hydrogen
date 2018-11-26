6d6daf90577c58ab49c5972bcd807bc7958f0a82
arch <aa>
2018-11-26 17:27:37 +0100

    load settings from json, start pygame window

--- loader/load_json_settings.py
+++ loader/load_json_settings.py
@@ -1,2 +1,9 @@
 import json
-json.
+
+
+def load_setting(file_name):
+    settings = None
+    with open(file_name) as file:
+        return json.load(file)
+
+
---
+++ test/load_test.py
@@ -0,0 +1,4 @@
+import unittest
+from loader import load_json_settings as ljs
+
+class Load_json_test()
